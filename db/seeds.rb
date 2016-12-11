f025bf8bc7ff5aea042b8f945c95ce90101e9982
Sergio Burgueño <burguer@Sergios-MacBook-Pro.local>
2016-12-11 15:23:22 -0800

    generating data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create_table "freelance_documents", force: :cascade do |t|
+#   t.string   "title"
+#   t.string   "description"
+#   t.text     "file_url"
+#   t.text     "image_url"
+#   t.datetime "created_at",  null: false
+#   t.datetime "updated_at",  null: false
+
+10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d}",
+    description: "Cold-pressed kogi woke fam, butcher messenger bag etsy franzen farm-to-table. Selfies street art pok pok chia. Irony humblebrag synth neutra drinking vinegar. ",
+    file_url: "http://www.mexicali.gob.mx/xxi/actas/21_1_10_31.pdf",
+    image_url: "http://lorempixel.com/output/business-q-g-640-480-5.jpg")
+end
